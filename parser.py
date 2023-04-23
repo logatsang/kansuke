ec89f17b5d9041f341e8b579810ef6d3403440dd
logatsang <tim.logatsang@gmail.com>
2023-04-23 03:12:20 -0400

    Identifying character primitives

---
+++ parser.py
@@ -0,0 +1,46 @@
+
+
+INPUT_FILENAME = "2023summer\kansuke\ids.txt"
+IDCS = "⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻"
+
+sequences_dict = {}
+visited = set()
+leaves = set()
+
+with open(INPUT_FILENAME, "r", encoding="utf-8") as input_file:
+    for line in input_file:
+        if line[0] == "#":
+            continue
+
+        line = line.strip().split()
+        codepoint, character, sequences = line[0], line[1], line[2:]
+        sequences_dict[character] = sequences
+
+
+def visit_char(char):
+    if char in visited:
+        return
+
+    visited.add(char)
+    if char not in sequences_dict:
+        leaves.add(char)
+        return
+
+    seqs = sequences_dict[char]
+    for seq in seqs:
+        if len(seq) == 1:
+            leaves.add(char)
+            continue 
+
+        for child in seq:
+            if child not in IDCS:
+                visit_char(child)
+            
+
+
+for character in sequences_dict:
+    visit_char(character)
+
+leaves = sorted(list(leaves))
+
+print("".join(leaves), len(leaves))
