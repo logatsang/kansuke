479bab025121c6a6c618397ff81b1cf1f33ba030
logatsang <tim.logatsang@gmail.com>
2023-04-26 03:11:09 -0400

    Tag some primitives + implemented tag addition

--- parser.py
+++ parser.py
@@ -1,7 +1,9 @@
 import re
 from collections import Counter
+from dataclasses import dataclass
 
-INPUT_FILENAME = "2023summer\kansuke\ids.txt"
+IDS_FILE = "ids.txt"
+TAG_FILE = "tags.txt"
 IDCS = "⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻"
 WRONG = "[\"AGHJKMOSTUVX\[\]αℓ①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮⑯⑲]"
 
@@ -9,7 +11,20 @@ sequences_dict = {}
 visited = set()
 leaves = Counter()
 
-with open(INPUT_FILENAME, "r", encoding="utf-8") as input_file:
+@dataclass
+class Character:
+    char: str
+    hor: int = 0
+    ver: int = 0
+    oth: int = 0
+
+    def __add__(self, other):
+        return Character(self.char,
+                         self.hor+other.hor,
+                         self.ver+other.ver,
+                         self.oth+other.oth)
+
+with open(IDS_FILE, "r", encoding="utf-8") as input_file:
     for line in input_file:
         if line[0] == "#":
             continue
@@ -42,10 +57,46 @@ def visit_char(char):
         for child in seq:
             if child not in IDCS:
                 visit_char(child)
-  
 
 
 for character in sequences_dict:
     visit_char(character)
 
-print(leaves.most_common(), len(leaves))
+
+tagged = {}
+visited.clear()
+with open(TAG_FILE, "r", encoding="utf-8") as input_file:
+    for line in input_file:
+        char, code, *raw = line.split()
+        hor, ver, oth = (int(x) for x in code.split("-"))
+        tagged[char] = Character(char, hor, ver, oth)
+
+
+for idc in IDCS:
+    tagged[idc] = Character(idc)
+
+
+def tag_char(char):
+    if char in tagged or len(sequences_dict[char]) == 1:
+        return
+    
+    for seq in sequences_dict[char]:
+        if any(9312 >= ord(x) >= 9330 for x in seq):
+            continue
+
+        for child in seq:
+            tag_char(child)
+        
+        if all(child in tagged for child in seq):
+            tagged[char] = Character(char)
+            for child in seq:
+                tagged[char] += tagged[child]
+            return  # TODO: allow multiple tags
+        
+
+for character in sequences_dict:
+    tag_char(character)
+
+print(tagged, len(tagged))
+
+# print(leaves.most_common(), len(leaves))
