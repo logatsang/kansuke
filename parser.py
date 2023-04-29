be8ff6225e6f47eba3fe86b6779eaef32298f685
logatsang <tim.logatsang@gmail.com>
2023-04-28 20:00:48 -0400

    Style

--- parser.py
+++ parser.py
@@ -1,37 +1,45 @@
+"""This module parses something."""     # TODO: proper docstring
+
 import re
 from collections import Counter
 from dataclasses import dataclass
-
-from typing import Tuple
 
 DEBUG = True
 
 IDS_FILE = "ids.txt"
 TAG_FILE = "tags.txt"
 IDCS = "⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻"
-WRONG = "[\"AGHJKMOSTUVX\[\]]"
+WRONG = "[\"AGHJKMOSTUVX[]]"
 
 visited = set()
 leaves = Counter()
 
+
+def is_unencoded(char: str) -> bool:
+    """Return whether a character represents an unencoded component."""
+    return 9312 <= ord(char) <= 9330
+
+
 @dataclass
 class Character:
-    "Class to represent a tagged character."
+    """Class to represent a tagged character."""
     char: str
     hor: int = 0
     ver: int = 0
     oth: int = 0
 
-    def __add__(self, other):
-        return Character(self.char,
-                         self.hor+other.hor,
-                         self.ver+other.ver,
-                         self.oth+other.oth)
+    def __add__(self, other: 'Character'):
+        return Character(
+            self.char,
+            self.hor+other.hor,
+            self.ver+other.ver,
+            self.oth+other.oth
+        )
 
 
 def read_ids(filename: str) -> dict:
-    "Read Ideographic Description Sequences from a specified file."
-    sequences_dict = dict()
+    """Read Ideographic Description Sequences from a specified file."""
+    sequences_dict = {}
     with open(filename, "r", encoding="utf-8") as input_file:
         for line in input_file:
             if line[0] == "#":
@@ -47,8 +55,8 @@ def read_ids(filename: str) -> dict:
 
 
 def read_tags(filename: str) -> dict:
-    "Read primitive tag data from a specified file."
-    tags = dict()
+    """Read primitive tag data from a specified file."""
+    tags = {}
     with open(filename, "r", encoding="utf-8") as input_file:
         for line in input_file:
             try:
@@ -57,12 +65,12 @@ def read_tags(filename: str) -> dict:
                 tags[char] = Character(char, hor, ver, oth)
             except ValueError:
                 continue
-    
+
     return tags
 
 
-def visit_char(ids_data: dict, char: str) -> None:
-    "Find all primitives contained in a character."
+def visit_char(ids_data: dict, char: str):
+    """Find all primitives contained in a character."""
     if char in visited:
         if char in leaves:
             leaves[char] += 1
@@ -74,19 +82,20 @@ def visit_char(ids_data: dict, char: str) -> None:
         return
 
     seqs = ids_data[char]
-    debug_seqs = list()
+    debug_seqs = []
+
     for seq in seqs:
         if len(seq) == 1:
             leaves[char] += 1
             continue
 
-        if DEBUG and any(9312 <= ord(x) <= 9330 for x in seq):
+        if DEBUG and any(is_unencoded(child) for child in seq):
             debug_seqs.append(seq)
 
         for child in seq:
-            if child not in IDCS and not (9312 <= ord(child) <= 9330):
+            if child not in IDCS and not is_unencoded(child):
                 visit_char(ids_data, child)
-        
+
     if DEBUG and debug_seqs:
         seqs_out = "\t".join(debug_seqs)
         with open("missing", "a", encoding="utf-8") as debug_file:
@@ -94,7 +103,7 @@ def visit_char(ids_data: dict, char: str) -> None:
 
 
 def tag_char(char: str, tagged: dict, ids_data: dict):
-    "Recursively tag a character and all of its components."
+    """Recursively tag a character and all of its components."""
     if char in visited:
         return
 
@@ -104,7 +113,7 @@ def tag_char(char: str, tagged: dict, ids_data: dict):
         return
 
     for seq in ids_data[char]:
-        if any(9312 <= ord(x) <= 9330 for x in seq):
+        if any(is_unencoded(x) for x in seq):
             continue
 
         for child in seq:
@@ -118,6 +127,7 @@ def tag_char(char: str, tagged: dict, ids_data: dict):
 
 
 def main():
+    """Main function"""
     # Read data
     ids_data = read_ids(IDS_FILE)
     tag_data = read_tags(TAG_FILE)
@@ -134,6 +144,7 @@ def main():
     visited.clear()
     for character in ids_data:
         tag_char(character, tag_data, ids_data)
-    
+
+
 if __name__ == "__main__":
     main()
